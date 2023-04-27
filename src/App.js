c38b546ce93e8cfe393ecba44a5887146d20e2a9
manishbachhavservicepackai <manish.b@servicepack.ai>
2023-04-27 13:06:10 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.get("https://manish.in.ngrok.io/accounts/login");
+      console.log(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          height: "100vh",
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <button
+          onClick={handleLogin}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Login
+        </button>
+      </div>
+
+    </>
   );
 }
 
